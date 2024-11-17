44f6eab31748931f60b622b68541a477060cc01b
Elizabeth O'Melia <eomelia@unc.edu>
2024-11-17 15:25:51 -0500

    Finished EX08!

--- exercises/ex08/linked_list.py
+++ exercises/ex08/linked_list.py
@@ -1,16 +1,19 @@
+"""EX08: Using Recursive Calls."""
+
 from __future__ import annotations
 
-
-"""Lesson on Linked Lists."""
 
 __author__: str = "730574218"
 
 
 class Node:
+    """Class object creation of Node."""
+
     value: int
     next: Node | None
 
     def __init__(self, value: int, next: Node | None):
+        """Initiation of class Node."""
         self.value = value
         self.next = next
 
@@ -72,6 +75,7 @@ print(last(courses))  # expect to print 301; recursive case
 
 
 def value_at(head: Node | None, index: int) -> int:
+    """Finds a Node at index and returns its value."""
     # Edge case:
     #   if the list is empty, raise IndexError
     # Base case:
@@ -84,79 +88,77 @@ def value_at(head: Node | None, index: int) -> int:
         return head.value
     else:  # recursive case
         rest: int = value_at(head.next, index - 1)
+        # subtract 1 from index for every loop b/c trying to progress toward base case
         return rest
 
 
-# Still working on it!
 def max(head: Node | None) -> int:
+    """Return the value of the Node with the maximum value in the linked list."""
+    # Edge case:
+    #   value of head is None
+    #   raise a ValueError
     if head is None:
         raise ValueError("Cannot call max with None.")
+    # Base case:
+    #   if it is the last Node
+    #   return its value
     if head.next is None:
         return head.value
+    # Recursive case:
+    #   set max_val to current Node value
+    #   recursively call head.next and compare to max_val
+    #   if larger, set max_val equal to it
+    #   return max_val
     else:
         max_val: int = head.value
         if max(head.next) > max_val:
             max_val = max(head.next)
-        return max_val  # current output is 20
-
-    # else:
-    # max_val: int = head.value
-    # if max_val < max(head.next):
-    #     max_val = head.value
-    # return max_val
+        return max_val
 
 
 def linkify(items: list[int]) -> Node | None:
+    """Assign Nodes the values of a list of ints."""
+    # Edge case:
+    #   an empty input list
+    #   return None
     if items == []:
         return None
-    head: Node = Node(items[0], None)
-    # if head.next is None:
-    #    rest: Node | None = linkify(items[])
+    # Base case:
+    #   when items has a length of 1
+    #   create Node with value = items[0] and next = None
     if len(items) == 1:
         head: Node = Node(items[0], None)
+    # Recursive case:
+    #   create Node with value = items[0] and
+    #   next = recursive call with slice subscription
+    #       slice subscription shortens list every time linkify is called
     else:
         head: Node = Node(items[0], linkify(items[1:]))
+    # print head using to_str function
+    #   print rather than return because return must be Node | None, not str
+    # return head, which is Node | None
     print(to_str(head))
     return head
-    # Go through list and create a Node for each item
-    #   Value should be
-    # Return Node that is connected to all the others
-    # head: Node = Node(items[0], None) # wrong!
-    # idx: int = 0
-    # head: Node | None = Node(items[idx], Node(idx + 1, None))
-
-    # idx: int = 0
-    # if idx == len(items) - 1:
-    #    current_val: int = items[idx]
-    #    new_node: Node | None = Node(current_val, None)
-    #    return new_node
-    # if idx < len(items):
-    #    current_val: int = items[idx]
-    #    items = items[1:]
-    #    new_node: Node | None = Node(current_val, linkify(items))
-    #    return new_node
-
-    # new_node: Node = Node(items[idx], linkify(items[]))
-    # num: int = 0
-
-    # rest: Node | None = linkify(items)
-    # if items == []:
-    # raise IndexError("List is empty.")
-    # else:
-    # start: int = 0
-    # end: int = len(items) + 1
-    # rest: Node | None = linkify(start + 1, end):
-    # return linkify([head.value, head.next.value])
 
 
 def scale(head: Node | None, factor: int) -> Node | None:
+    """Scale a linked list by a specified factor."""
+    # Edge case:
+    #   if value of head is None
+    #   return None
     if head is None:
         return None
+    # Base case:
+    #   if last Node in linked list
+    #   return Node with value scaled by factor and next = None
     if head.next is None:
-        return None
+        return Node(head.value * factor, None)
+    # Recursive case:
+    #   recursively call scale for variable rest
+    #   print a Node with a scaled value and next = rest
+    #       use to_str to convert to a str
+    #   return the same Node without being converted to a str
     else:
-        first: int = head.value * factor
         rest: Node | None = scale(head.next, factor)
-        #    head.value = head.value * factor
-        #    scaled: Node | None = scale(head.next, factor)
-        return Node(first, rest)
+        print(to_str(Node(head.value * factor, rest)))
+        return Node(head.value * factor, rest)
